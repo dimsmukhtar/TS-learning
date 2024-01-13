a713b0a1ac3be3912560e0f2bdd5af3b6f0a4882
Dimas-Mukhtar <95218465+Dimas-Mukhtar@users.noreply.github.com>
2024-01-14 04:00:16 +0700

    first commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/consistent-type-imports */
+import express, { Application, NextFunction, Request, Response } from 'express'
+
+const app: Application = express()
+
+const PORT: number = 3000
+
+app.use('/health', (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).json({
+    message: 'Hello world'
+  })
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`)
+})
